1034bad1b045b14ede89fb6d3648c21317b0f0ef
Matt Liu <mattliuym@outlook.com>
2024-08-12 20:49:50 +1200

    Updated unit test

--- api/test/services/line-items/line-items.test.ts
+++ api/test/services/line-items/line-items.test.ts
@@ -1,11 +1,19 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.test.html
-import assert from "assert"
-import { app } from "../../../src/app"
+import assert from 'assert'
+import { app } from '../../../src/app'
 
-describe("line-items service", () => {
-  it("registered the service", () => {
-    const service = app.service("line-items")
+describe('line-items service', () => {
+  it('registered the service', () => {
+    const service = app.service('line-items')
 
-    assert.ok(service, "Registered the service")
+    assert.ok(service, 'Registered the service')
+  })
+
+  it('test line-items service', async () => {
+    const service = app.service('line-items')
+
+    const result = await service.find()
+
+    assert.ok(result, 'line-items service working')
   })
 })
--- api/test/services/orders/orders.test.ts
+++ api/test/services/orders/orders.test.ts
@@ -1,11 +1,25 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.test.html
-import assert from "assert"
-import { app } from "../../../src/app"
+import assert from 'assert'
+import { app } from '../../../src/app'
 
-describe("orders service", () => {
-  it("registered the service", () => {
-    const service = app.service("orders")
+describe('orders service', () => {
+  it('registered the service', () => {
+    const service = app.service('orders')
 
-    assert.ok(service, "Registered the service")
+    assert.ok(service, 'Registered the service')
+  })
+
+  it('create/finish an order', async () => {
+    const service = app.service('orders')
+
+    const order = await service.create({})
+
+    assert.strictEqual(order.status, 'scanning')
+    assert.strictEqual(order.total, '0.00')
+
+    const oderComplete = await service.patch(order.id, { status: 'completed' })
+
+    assert.strictEqual(oderComplete.status, 'completed')
+    assert.strictEqual(oderComplete.total, '0.00')
   })
 })
